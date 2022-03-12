7ec6f015bc26ca2122af6b331d17708594538cfe
Dovar <dovar21@gmail.com>
2022-03-12 09:47:49 +0500

    Add base repository class and interface

---
+++ EwalletAlifTech/Modules/Core/Repositories/IRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EwalletAlifTech.Modules.Core.Repositories
+{
+    public interface IRepositoryBase<T>
+    {
+        Task<T> FindByIdAsync(Guid id);
+        Task<T> Create(T entity);
+        Task Update(T entity);
+        Task SaveAsync();
+
+    }
+}
---
+++ EwalletAlifTech/Modules/Core/Repositories/RepositoryBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EwalletAlifTech.Modules.Core.Repositories
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T: class
+    {
+        protected readonly AppDbContext _dbContext;
+
+        public RepositoryBase(AppDbContext appDbContext)
+        {
+            _dbContext = appDbContext;
+        }
+        public async Task<T> FindByIdAsync(Guid id)
+        {
+            return await this._dbContext.Set<T>().FindAsync(id);
+        }
+        public async Task<T> Create(T entity)
+        {
+            this.ChangeStatusToAdd(entity);
+
+            await this.SaveAsync();
+
+            return entity;
+        }
+        public async Task Update(T entity)
+        {
+            this.ChangeStatusToUpdate(entity);
+
+            await this.SaveAsync();
+        }
+        public async Task SaveAsync()
+        {
+            await this._dbContext.SaveChangesAsync();            
+        }
+        protected void ChangeStatusToAdd(T entity)
+        {
+            this._dbContext.Set<T>().Add(entity);
+        }
+        protected void ChangeStatusToUpdate(T entity)
+        {
+            this._dbContext.Set<T>().Update(entity);
+        }
+    }
+}
