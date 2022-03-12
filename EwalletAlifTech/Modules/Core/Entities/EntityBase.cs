8f1c6000fb331c316dc45b5f8ef9011e003924c5
Dovar <dovar21@gmail.com>
2022-03-12 09:45:37 +0500

    Add base entity class and interface

---
+++ EwalletAlifTech/Modules/Core/Entities/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EwalletAlifTech.Modules.Core.Entities
+{
+    public class EntityBase : IEntityBase
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ EwalletAlifTech/Modules/Core/Entities/IEntityBase.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EwalletAlifTech.Modules.Core.Entities
+{
+    public interface IEntityBase
+    {
+        Guid Id { get; set; }
+    }
+}
