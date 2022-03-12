8eeaba3781173adaba201c5161403b66ec427f22
Dovar <dovar21@gmail.com>
2022-03-12 09:46:00 +0500

    Add core enums

---
+++ EwalletAlifTech/Modules/Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace EwalletAlifTech.Modules.Core.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this object obj)
+        {
+            if (obj == null)
+            {
+                return "";
+            }
+
+            var display = obj.GetType()
+                .GetField(obj.ToString()!)?
+                .GetCustomAttributes(typeof(DisplayAttribute), false)
+                .Cast<DisplayAttribute>()
+                .LastOrDefault();
+
+            return display?.GetName() ?? "";
+        }
+    }
+}
