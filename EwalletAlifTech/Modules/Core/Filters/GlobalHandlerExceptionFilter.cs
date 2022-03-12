503ed6e041b0eedfb3ed1c3675aa7eea858e9b93
Dovar <dovar21@gmail.com>
2022-03-12 09:45:49 +0500

    Add core filters

---
+++ EwalletAlifTech/Modules/Core/Filters/GlobalHandlerExceptionFilter.cs
@@ -0,0 +1,45 @@
+﻿using EwalletAlifTech.Modules.Core.Exceptions;
+using EwalletAlifTech.Modules.Core.Responses;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace EwalletAlifTech.Modules.Core.Filters
+{
+    public class GlobalHandlerExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {   
+            if (context.Exception is ResourceNotFoundException)
+            {
+                context.Result = new ErrorJsonObjectResult(code: (short) HttpStatusCode.RESOURCE_NOT_FOUND, message:context.Exception.Message);
+                context.ExceptionHandled = true;
+            }
+            else if (context.Exception is ValidationException)
+            {
+                context.Result = new ErrorJsonObjectResult(code: (short)HttpStatusCode.VALIDATION_EXCEPTION, message: context.Exception.Message);
+
+                context.ExceptionHandled = true;
+            }
+            else if (context.Exception is LogicException)
+            {
+                context.Result = new ErrorJsonObjectResult(code: (short)HttpStatusCode.UNKNOWN_ERROR, message: context.Exception.Message);
+                context.ExceptionHandled = true;
+            }
+            else if (context.Exception is AccessForbiddenException)
+            {
+                context.Result = new ErrorJsonObjectResult(code: (short)HttpStatusCode.FORBIDDEN, message: context.Exception.Message);
+                context.ExceptionHandled = true;
+            }
+            else if (context.Exception != null)
+            {
+                var loggerFactory = context.HttpContext.RequestServices.GetService<ILoggerFactory>();
+                var logger = loggerFactory.CreateLogger(nameof(GlobalHandlerExceptionFilter));
+                logger.LogError($"Message: {context.Exception.Message}, Trace:{context.Exception.StackTrace}");
+
+                context.Result = new ErrorJsonObjectResult(code: (short)HttpStatusCode.UNKNOWN_ERROR, message: "Unknown error.");
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
---
+++ EwalletAlifTech/Modules/Core/Filters/ValidateModelAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using EwalletAlifTech.Modules.Core.Responses;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EwalletAlifTech.Modules.Core.Filters
+{
+    public class ValidateModelAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext actionContext)
+        {
+            if (actionContext.ModelState.IsValid == false)
+            {
+                object errors = actionContext.ModelState.Values
+                    .SelectMany(e => e.Errors)
+                    .Select(e => e.ErrorMessage);
+
+                actionContext.Result =
+                    new ErrorJsonObjectResult(1, errors: errors, message: "Some fields are incorrect");
+            }
+
+        }
+    }
+}
