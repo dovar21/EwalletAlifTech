f356359e98e3fdc59ac5997667dd77a666b07274
Dovar <dovar21@gmail.com>
2022-03-12 09:54:13 +0500

    Add transaction request models

---
+++ EwalletAlifTech/Modules/Transactions/ModelRequests/CreateTransactionRequestModel.cs
@@ -0,0 +1,11 @@
+﻿using EwalletAlifTech.Modules.Users.Entities;
+
+namespace EwalletAlifTech.Modules.Transactions.ModelRequests
+{
+    public class CreateTransactionRequestModel
+    {
+        public User FromUser { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ EwalletAlifTech/Modules/Transactions/ModelRequests/TransactionHistoryRequestModel.cs
@@ -0,0 +1,18 @@
+﻿using EwalletAlifTech.Modules.Transactions.Enums;
+using Newtonsoft.Json;
+using System;
+
+namespace EwalletAlifTech.Modules.Transactions.ModelRequests
+{
+    public class TransactionHistoryRequestModel
+    {
+        [JsonProperty("start_date", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTime? StartDate { get; set; }
+
+        [JsonProperty("end_date", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTime? EndDate { get; set; }
+
+        [JsonProperty("transaction_type", NullValueHandling = NullValueHandling.Ignore)]
+        public TransactionType TransactionType { get; set; }
+    }
+}
