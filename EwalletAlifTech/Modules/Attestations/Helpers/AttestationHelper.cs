6a0b97933960ccb5d440fdb0cfdfff0e94bb3dbf
Dovar <dovar21@gmail.com>
2022-03-12 09:44:19 +0500

    Add AttestationHelper class

---
+++ EwalletAlifTech/Modules/Attestations/Helpers/AttestationHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EwalletAlifTech.Modules.Users.Modules.Attestations.Helpers
+{
+    public static class AttestationHelper
+    {
+        public static readonly Guid NOT_IDENTIFIED = new ("5ee95dcb-a078-11e8-904b-b06ebfbfa234");
+        public static readonly Guid IDENTIFIED = new ("5ee95dcb-a078-11e8-904b-b06ebfbfa234");
+    }
+}
