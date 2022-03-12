c38569491db4eb0fa1f6992fb68aa0c655c6f293
Dovar <dovar21@gmail.com>
2022-03-12 09:47:18 +0500

    Add core middlewares

---
+++ EwalletAlifTech/Modules/Core/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+using EwalletAlifTech.Modules.Core.Exceptions;
+using EwalletAlifTech.Modules.Core.Responses;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace EwalletAlifTech.Modules.Core.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (ValidationException e)
+            {
+                var result = new JsonResponseObject(code: (int)HttpStatusCode.VALIDATION_EXCEPTION, message: e.Message);
+
+                await this.ExceptionResponse(context, result);
+            }
+            catch (AccessForbiddenException e)
+            {
+                var result = new JsonResponseObject(code: (int) HttpStatusCode.FORBIDDEN,
+                    message: e.Message);
+                await this.ExceptionResponse(context, result);
+            }
+            catch (LogicException e)
+            {
+                var result = new JsonResponseObject(code: (int)HttpStatusCode.UNKNOWN_ERROR,
+                    message: e.Message);
+                await this.ExceptionResponse(context, result);
+            }
+            catch (Exception e)
+            {
+                var result = new JsonResponseObject(code: (int)HttpStatusCode.UNKNOWN_ERROR,
+                    message: "Unknown error.");
+
+                var loggerFactory = context.RequestServices.GetService<ILoggerFactory>();
+                var logger = loggerFactory.CreateLogger(nameof(ErrorHandlingMiddleware));
+                logger.LogError($"Message: {e.Message}, Trace:{e.StackTrace}");
+
+                await this.ExceptionResponse(context, result);
+            }
+
+        }
+        private async Task ExceptionResponse(HttpContext context,JsonResponseObject result)
+        {
+            context.Response.StatusCode = 400;
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(result));
+        }
+    }
+}
---
+++ EwalletAlifTech/Modules/Core/Middlewares/LoggingContextMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using EwalletAlifTech.Modules.Core.Exceptions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog.Context;
+
+namespace EwalletAlifTech.Modules.Core.Middlewares
+{
+    public class LoggingContextMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LoggingContextMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var appName = context.RequestServices.GetRequiredService<IConfiguration>().GetSection("App")["name"];
+            
+            if(string.IsNullOrEmpty(appName))
+                throw new LogicException("AppName not found");
+            
+            using (LogContext.PushProperty("AppName", appName))
+            {
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
