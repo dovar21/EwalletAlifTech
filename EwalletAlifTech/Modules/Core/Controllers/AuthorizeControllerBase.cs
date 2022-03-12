2140ed99a1db7f36da0cf0c22de80ea45ffe02d4
Dovar <dovar21@gmail.com>
2022-03-12 09:45:19 +0500

    Add base controller

---
+++ EwalletAlifTech/Modules/Core/Controllers/AuthorizeControllerBase.cs
@@ -0,0 +1,29 @@
+﻿using EwalletAlifTech.Modules.Core.Exceptions;
+using EwalletAlifTech.Modules.DigestAuthentication.Implementation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EwalletAlifTech.Modules.Core.Controllers
+{
+    /// <summary>
+    /// Base controller class
+    /// </summary>
+    [Authorize(AuthenticationSchemes = "Digest")]
+    public abstract class AuthorizeControllerBase : ControllerBase
+    {
+        /// <summary>
+        /// Get authorized user
+        /// </summary>
+        /// <returns>Username</returns>
+        public string GetAuthorizedUsername()
+        {
+            if (!Request.Headers.TryGetValue(DigestAuthImplementation.AuthorizationHeaderName, out var headerValue))
+                throw new AccessForbiddenException();
+
+            if (!DigestChallengeResponse.TryParse(headerValue, out var challengeResponse))
+                throw new AccessForbiddenException();
+
+            return challengeResponse.Username;
+        }
+    }
+}
