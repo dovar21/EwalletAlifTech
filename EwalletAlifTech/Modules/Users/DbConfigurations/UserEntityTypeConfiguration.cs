dfdca983dfbe183ef3950fd0b63d83540f4d8301
Dovar <dovar21@gmail.com>
2022-03-12 09:52:31 +0500

    Add user entity and entity configration classes

---
+++ EwalletAlifTech/Modules/Users/DbConfigurations/UserEntityTypeConfiguration.cs
@@ -0,0 +1,80 @@
+﻿using EwalletAlifTech.Modules.Users.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace EwalletAlifTech.Modules.Users.DbConfigurations
+{
+    public class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(a => a.Id);
+
+            builder.HasMany(a => a.Accounts)
+                .WithOne(a => a.User);
+
+            builder.HasOne(a => a.Attestation)
+                .WithMany(a => a.Users).HasForeignKey(a => a.AttestationId);
+
+            builder.Property(a => a.AttestationId).HasColumnName("attestation_id");
+            builder.Property(a => a.UserName).HasMaxLength(50).IsRequired();
+            builder.Property(a => a.PhoneNumber).HasColumnName("phone_number").HasMaxLength(15).IsRequired();
+            builder.Property(a => a.Email).HasMaxLength(50).IsRequired();
+            builder.Property(a => a.Password).IsRequired();
+            builder.Property(a => a.FullName).HasColumnName("full_name").HasMaxLength(50).IsRequired();
+            builder.Property(b => b.CreatedAt).HasColumnName("created_at").IsRequired();
+            builder.Property(b => b.UpdatedAt).HasColumnName("updated_at").IsRequired();
+            builder.Property(b => b.IsActive).HasColumnName("is_active").IsRequired();
+
+            builder.HasAlternateKey(a => a.UserName);
+            builder.HasAlternateKey(a => a.PhoneNumber);
+            builder.HasAlternateKey(a => a.Email);
+
+            builder.ToTable("users").HasData(
+                     new User
+                     {
+                         Id = Guid.Parse("1ca06b7b-13fa-5952-827b-2fef6e40d111"),
+                         UserName = "user1",
+                         PhoneNumber = "992938640102",
+                         Email = "user1@gmail.com",
+                         Password = "user1",
+                         Hash = "",
+                         FullName = "User1",
+                         AttestationId = Guid.Parse("5ee95dcb-a078-11e8-904b-b06ebfbfa235"),
+                         CreatedAt = DateTime.Now,
+                         UpdatedAt = DateTime.Now,
+                         IsActive = true
+                     },
+                     new User
+                     {
+                         Id = Guid.Parse("1ca06b7b-13fa-5952-827b-2fef6e40d112"),
+                         UserName = "user2",
+                         PhoneNumber = "992938640103",
+                         Email = "user2@gmail.com",
+                         Password = "user2",
+                         Hash = "",
+                         FullName = "User2",
+                         AttestationId = Guid.Parse("5ee95dcb-a078-11e8-904b-b06ebfbfa234"),
+                         CreatedAt = DateTime.Now,
+                         UpdatedAt = DateTime.Now,
+                         IsActive = true
+                     },
+                     new User
+                     {
+                         Id = Guid.Parse("1ca06b7b-13fa-5952-827b-2fef6e40d113"),
+                         UserName = "user3",
+                         PhoneNumber = "992938640104",
+                         Email = "user3@gmail.com",
+                         Password = "user3",
+                         Hash = "",
+                         FullName = "User3",
+                         AttestationId = Guid.Parse("5ee95dcb-a078-11e8-904b-b06ebfbfa235"),
+                         CreatedAt = DateTime.Now,
+                         UpdatedAt = DateTime.Now,
+                         IsActive = true
+                     }
+                );
+        }
+    }
+}
---
+++ EwalletAlifTech/Modules/Users/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using EwalletAlifTech.Modules.Core.Entities;
+using EwalletAlifTech.Modules.Users.Modules.Attestations.Entities;
+using EwalletAlifTech.Modules.Accounts.Entities;
+using EwalletAlifTech.Modules.Transactions.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EwalletAlifTech.Modules.Users.Entities
+{
+    public class User : EntityBase
+    {
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Hash { get; set; }
+        public string FullName { get; set; }
+        public Guid AttestationId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsActive { get; set; }
+        [NotMapped]
+        public List<Account> Accounts { get; set; }
+        [NotMapped]
+        public List<Transaction> Transactions { get; set; }
+        public Attestation Attestation { get; set; }
+    }
+}
