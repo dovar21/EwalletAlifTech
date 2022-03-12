e3a96b61ecec7dcb7e4c838b208e1282b72af998
Dovar <dovar21@gmail.com>
2022-03-12 09:47:33 +0500

    Add core helper class

---
+++ EwalletAlifTech/Modules/Core/Helpers/BootstrapBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace EwalletAlifTech.Modules.Core.Helpers
+{
+    public abstract class BootstrapBase
+    {
+        protected abstract string GetCurrentNamespace();
+
+        public string GetPathSettings(string filenameSettings = "settings.json")
+        {
+            string currentNamespace =
+                this.GetCurrentNamespace();
+
+            string[] fullPath = currentNamespace.Split(".");
+
+            var relativePath = fullPath.Where(p => p != fullPath[0]).ToArray();
+
+            Array.Resize(ref relativePath, relativePath.Length + 1);
+            relativePath[relativePath.Length - 1] = filenameSettings;
+            return Path.Combine(relativePath);
+        }
+    }
+}
