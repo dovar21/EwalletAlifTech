141d9627b9deb4cfdbda1ed6a17e2fd19b7f96b7
Dovar <dovar21@gmail.com>
2022-03-12 09:43:31 +0500

    Add attestation entity class

---
+++ EwalletAlifTech/Modules/Attestations/Entities/Attestation.cs
@@ -0,0 +1,18 @@
+﻿using EwalletAlifTech.Modules.Core.Entities;
+using EwalletAlifTech.Modules.Users.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EwalletAlifTech.Modules.Users.Modules.Attestations.Entities
+{
+    public class Attestation : EntityBase
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        [NotMapped]
+        public List<User> Users { get; set; } 
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
