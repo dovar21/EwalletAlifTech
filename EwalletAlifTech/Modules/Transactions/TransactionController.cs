4ea4e5f43de9bbbaddac38f3facefda7925a2c53
Dovar <dovar21@gmail.com>
2022-03-12 09:57:54 +0500

    Add transactions controller

---
+++ EwalletAlifTech/Modules/Transactions/TransactionController.cs
@@ -0,0 +1,28 @@
+﻿using EwalletAlifTech.Modules.Core.Controllers;
+using EwalletAlifTech.Modules.Core.Responses;
+using EwalletAlifTech.Modules.Transactions.ModelRequests;
+using EwalletAlifTech.Modules.Transactions.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EwalletAlifTech.Modules.Accounts
+{
+    [Route("api/transactions")]
+    public class TransactionController : AuthorizeControllerBase
+    {
+        private readonly ITransactionService _transactionService;
+
+        public TransactionController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+        [Route("history")]
+        [HttpPost]
+        public async Task<IActionResult> ReplenishEwalletAccount([FromBody] TransactionHistoryRequestModel model)
+        {
+            var data = await _transactionService.GetHistory(model, GetAuthorizedUsername());
+
+            return new SuccessJsonObjectResult(data: data);
+        }
+    }
+}
