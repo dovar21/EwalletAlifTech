706ba8003b5c4f7197a1931bbba6c7cf425f427d
Dovar <dovar21@gmail.com>
2022-03-12 09:53:04 +0500

    Add user repository interface and class

---
+++ EwalletAlifTech/Modules/Users/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using EwalletAlifTech.Modules.Users.Entities;
+using EwalletAlifTech.Modules.Core.Repositories;
+
+namespace EwalletAlifTech.Modules.Users.Repositories
+{
+    public interface IUserRepository : IRepositoryBase<User>
+    {
+        new Task<User> FindByIdAsync(Guid id);
+        Task<User> FindByUsernameAsync(string username);
+        Task<User> FindByPhoneNumberAsync(string phoneNumber);
+    }
+}
---
+++ EwalletAlifTech/Modules/Users/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EwalletAlifTech.Modules.Core.Repositories;
+using EwalletAlifTech.Modules.Users.Entities;
+using EwalletAlifTech.Modules.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace EwalletAlifTech.Modules.Users.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+        public UserRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+
+        }
+        public new async Task<User> FindByIdAsync(Guid id)
+        {
+            return await _dbContext.Users.Where(u => u.Id == id).SingleOrDefaultAsync();
+        }
+        public async Task<User> FindByUsernameAsync(string username)
+        {
+            return await _dbContext.Users.Where(u => u.UserName == username && u.IsActive).SingleOrDefaultAsync();
+        }
+        public async Task<User> FindByPhoneNumberAsync(string phoneNumber)
+        {
+            return await _dbContext.Users.Where(u => u.PhoneNumber == phoneNumber && u.IsActive).SingleOrDefaultAsync();
+        }
+    }
+}
