eb857f3e7ed1a8f252b912e133a6939b67f7f7d9
Dovar <dovar21@gmail.com>
2022-03-12 09:39:16 +0500

    Add default controller

---
+++ EwalletAlifTech/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EwalletAlifTech.Controllers
+{
+    [Route("api/home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok("Started");
+        }
+    }
+}
