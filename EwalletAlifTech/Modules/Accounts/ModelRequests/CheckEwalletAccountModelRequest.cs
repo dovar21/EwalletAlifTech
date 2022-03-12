1486fb91a67396e6b7fe6a415509ba60601c9c80
Dovar <dovar21@gmail.com>
2022-03-12 09:55:35 +0500

    Add account request models

---
+++ EwalletAlifTech/Modules/Accounts/ModelRequests/CheckEwalletAccountModelRequest.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace EwalletAlifTech.Modules.Accounts.ModelRequests
+{
+    public class CheckEwalletAccountModelRequest
+    {
+        [Required(ErrorMessage = "Phone number is required")]
+        [JsonProperty("phone_number")]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ EwalletAlifTech/Modules/Accounts/ModelRequests/ReplenishEwalletAccountModelRequest.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace EwalletAlifTech.Modules.Accounts.ModelRequests
+{
+    public class ReplenishEwalletAccountModelRequest
+    {
+        [Required(ErrorMessage = "Phone number is required")]
+        [JsonProperty("phone_number")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Amount is required")]
+        [JsonProperty("amount")]
+        public decimal Amount { get; set; }
+    }
+}
